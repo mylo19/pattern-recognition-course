11e8352c33c22c63994837277fcb7c0ee2925b9c
mylo19 <mylowade98@gmail.com>
2021-11-18 23:49:13 +0200

    quiz 4

---
+++ Quiz 4/fourthAssignment.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from sklearn.neural_network import MLPRegressor
+import numpy as np
+
+X1 = [2, 2, -2, -2, 1, 1, -1, -1]
+X2 = [2, -2, -2, 2, 1, -1, -1, 1]
+Y = [1, 1, 1, 1, 2, 2, 2, 2]
+alldata = pd.DataFrame({"X1":X1, "X2":X2, "Y":Y})
+#print(alldata)
+xtrain = alldata.loc[:, ["X1", "X2"]]
+ytrain = alldata.loc[:, "Y"]
+#print(ytrain)
+
+clf = MLPRegressor(hidden_layer_sizes=(20, 20), max_iter=10000) #depending on the question, change the hidden layer size
+clf = clf.fit(xtrain, ytrain)
+
+# Training Error
+pred = clf.predict(xtrain)
+trainingError = [(t - p) for (t, p) in zip(ytrain, pred)]
+MAE = np.mean(np.abs(trainingError))
+print(MAE)
+
--- Quiz 4/fourthLab.py
+++ Quiz 4/fourthLab.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.neural_network import MLPRegressor
-import  numpy as np
+import numpy as np
 
 """""anndata = pd.DataFrame({"X1": [0, 0, 1, 1], "X2": [0, 1, 0, 1], "Y": [1, 1, -1, -1]})
 X = anndata.loc[:, ["X1", "X2"]]
@@ -17,7 +17,7 @@ clf = clf.fit(X, y)
 print("point [0, 0] ->", clf.predict([[0, 0]]))
 print("point [1, 0] ->", clf.predict([[1, 0]]))
 print("point [0.5, 0] ->", clf.predict([[0.5, 0]]))
-
+"""""
 alldata = pd.read_csv("./alldata.txt")
 #print(alldata)
 xtrain = alldata.loc[0:600, ["X1", "X2"]]
@@ -28,7 +28,7 @@ ytest = alldata.loc[600:800, "y"]
 
 plt.scatter(xtrain[(ytrain == 2)].X1, xtrain[(ytrain == 2)].X2, c="red", marker="+")
 plt.scatter(xtrain[(ytrain == 1)].X1, xtrain[(ytrain == 1)].X2, c="blue", marker="o")
-plt.show() """""
+plt.show()
 
 clf = MLPRegressor(hidden_layer_sizes= (500, 20), tol=0.01)
 clf = clf.fit(xtrain, ytrain)
