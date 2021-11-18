d86ae926cad1587db36a7f67a4ef8bc50ad0241b
mylo19 <mylowade98@gmail.com>
2021-11-19 00:01:11 +0200

    warning removed, trained with values

--- Quiz 4/fourthAssignment.py
+++ Quiz 4/fourthAssignment.py
@@ -9,14 +9,16 @@ alldata = pd.DataFrame({"X1": X1, "X2": X2, "Y": Y})
 # print(alldata)
 xtrain = alldata.loc[:, ["X1", "X2"]]
 ytrain = alldata.loc[:, "Y"]
-# print(ytrain)
+print(ytrain.values)
 
 clf = MLPRegressor(hidden_layer_sizes=(20, 20),
                    max_iter=10000)  # depending on the question, change the hidden layer size
-clf = clf.fit(xtrain, ytrain)
+clf = clf.fit(xtrain.values, ytrain.values)
 
 # Training Error
-pred = clf.predict(xtrain)
+pred = clf.predict(xtrain.values)
 trainingError = [(t - p) for (t, p) in zip(ytrain, pred)]
 MAE = np.mean(np.abs(trainingError))
 print(MAE)
+
+print("point->", clf.predict([[0.8, -0.6]]))
