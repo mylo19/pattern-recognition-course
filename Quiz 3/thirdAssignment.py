f8aa18b861fe74e4a8a1684656b03bf8ab7b7813
mylo19 <mylowade98@gmail.com>
2021-11-24 19:02:42 +0200

    correct imports

--- Quiz 3/thirdAssignment.py
+++ Quiz 3/thirdAssignment.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_curve, auc
+from sklearn.metrics import f1_score, roc_curve, auc
 import matplotlib.pyplot as plt
 
 data = pd.read_csv("./quiz_data.csv")
